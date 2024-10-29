0bc42a3d2f0adf144cf3eff85fed2c2be8b0d43f
Juliana9b9 <jpachec2@mail.umw.edu>
2024-10-28 23:41:28 -0400

    Game
    Finished product.

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,66 +1,93 @@
 let debug = true;
-let x = 0; 
+let x = 0;
 let y = 0;
-let d = 0;
+let d = 0; 
 let score = 0;
-let speedfactor = 3;  
+let speedfactor = 3; 
 let speedx = speedfactor;
-let speedy = speedfactor;
+let speedy = speedfactor; 
+let goalx = 0;
+let goaly = 0;
+let goalSize = 30;
+
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800,800);
 
   x = random(width);
-  y = random(height); 
+  y = random(height);
+
+  goalx = random(width);
+  goaly = random(height);
 }
 
 function draw() {
-  background(220);
+  background(200);
 
-  //Distance formula
+  rect(goalx, goaly, goalSize, goalSize);
+  fill("green");
+  noStroke();
+
+//distance formula
   d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
 
-  //The movement 
-  x += speedx;
-  y == speedy;
+  x += speedx; //add the movement on x-axis (left to right)
+  y += speedy; //add the movement on y-axis (up and down)
 
-  //draw shape for the follower, in this case I chose a circle. 
-  circle(x,y,50);
-  fill(0,225,225); 
+  //Draw circle for the follower
+  circle(x, y, 50); 
+  fill("red");
+  noStroke();
 
-  if (mouseX > x){ 
-    //move to the right
+  if (mouseX > x ){ //move to the right
+    
     speedx = speedfactor;
-  }else{ 
-    //move to the left
-    speedx = -speedfactor; 
+
+  }else{ //move left 
+    
+    speedx = -speedfactor;
   }
 
-  if (mouseY > y){ 
-    //move to the right
+  if (mouseY > y ){ //move to the right
+    
     speedy = speedfactor;
-  }else{ 
-    //move to the left
-    speedy = -speedfactor; 
+
+  }else{ //move left 
+    
+    speedy = -speedfactor;
   }
 
-//Check for collision 
-  if (d < 25){
-    score += 1; 
+  if (d < 25){ //check for collison with follower
+    score -= 1;
+    x = random(width);
+    y = random(height);
+  }
+
+  //check for collision with square
+  if (
+    mouseX > goalx &
+    mouseX < goalx + goalSize & 
+    mouseY > goaly & 
+    mouseY < goaly + goalSize 
+    ){
+    score += 1;
+    //reset follower
     x = random(width);
     y = random(height);
 
-
+    //reset goal
+    goalx = random(width);
+    goaly = random(height);
   }
 
-  if (debug){
-    textsize(30);
-    text("mouseX: " + mouseX, 50, 50);
-    text("mouseY: " + mouseY, 50, 80);
-    text("x: " + x, 50, 120);
-    text("y: " + y, 50, 150);
-    text("d: " + d, 50, 180);
-    text("score: " + score, 50, 210);
 
+  if (debug){
+    textSize(30);
+    //text("mouseX: " + mouseX, 50,50);
+    //text("mouseY: " + mouseY, 50,80);
+    //text("x: " + x, 50, 120);
+    //text("x: " + x, 50, 150);
+    //text("d: " + d, 50, 180);
+    text("score: " + score, 50, 210); 
   }
 }
